fe85f73af4b119e8cbfd46b79da8363a01cbc5e2
Thierry Gameiro <thierry.gmr@outlook.fr>
2020-09-10 19:15:51 +0200

    corrections functions

--- utils/eda.py
+++ utils/eda.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def apply_gender(all_gender, threshold=0.7, apply=None):
@@ -20,3 +21,15 @@ def apply_gender(all_gender, threshold=0.7, apply=None):
         return serie
     else : 
         return sexe
+
+
+def plot_na(pourc_na, df, color=None):
+    plt.figure(figsize=(12, 8))
+    plt.title('Part des NA pour chaque variable – Table Patient')
+    plt.barh(df.columns, pourc_na,
+             color=color)
+    plt.xlabel('Pourcentage de valeurs manquantes')
+    plt.ylabel('Variables')
+
+    for i, v in enumerate(pourc_na):
+        plt.text(v, i+0.2, " "+str(round(v, 2))+"%", color='blue', va='center')
